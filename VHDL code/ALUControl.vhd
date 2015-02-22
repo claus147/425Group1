84e661caf4a88ea620f8ef9c610ea41939009676
unheimlich <andrew.d.jesson@gmail.com>
2015-02-22 14:26:06 -0500

    Update ALUControl.vhd
    CLAUS email me andrew.d.jesson@gmail.com

--- VHDL code/ALUControl.vhd
+++ VHDL code/ALUControl.vhd
@@ -3,11 +3,11 @@
 -- 
 -- Inputs:
 --	- Funct	6-bit operand
---	- ALUOp 2-bit contol
+--	- ALUOp 4-bit contol
 --
 -- Outputs:
 --				
--- 	- Op		5-bit control	(for ALU)
+-- 	- Op		6-bit control	(for ALU)
 --		
 -- Control:
 -- 	- clk		clock
@@ -26,9 +26,9 @@ entity ALUControl is
 	port (
 		clk 					: in std_logic;
 		rst 					: in std_logic;
-		ALUOp					: in unsigned(1 downto 0);
+		ALUOp					: in unsigned(3 downto 0);
 		Funct					: in unsigned(5 downto 0);
-		Op					: out unsigned(4 downto 0);
+		Op					: out unsigned(5 downto 0);
 	);
 end entity ALUControl;
 
@@ -40,7 +40,49 @@ begin
 
 	if(rising_edge(clk)) then
 		
+		case ALUOp is
 		
+			when "0000" -- R Instruction
+			
+				Op <= Funct;
+				
+			when "0001" -- addi,lw,lb,sw,sb (do A + B in ALU)
+			
+				Op <= "100000";
+				
+			when "0010" -- beq,bne (do A-B in ALU)
+			
+				Op <= "100010";
+				
+			when "0011" -- slti (do slt(A,B) in ALU)
+			
+				Op <= "101010";
+				
+			when "0100" -- andi (do A and B in ALU)
+			
+				Op <= "100100";
+				
+			when "0101" -- ori (do A or B in ALU)
+			
+				Op <= "100101";
+				
+			when "0110" -- xori (do A xor B in ALU)
+			
+				Op <= "100110";
+				
+			when "0111" -- lui (do lui in ALU)
+			
+				--Op <= "foo"; define lui in ALU
+				
+			when "1000" -- jal
+			
+				-- TODO maybe not necessary
+				
+			when "1001" -- j
+			
+				-- TODO logic external to ALU
+		
+		end case;
 		
 	end if;
 
