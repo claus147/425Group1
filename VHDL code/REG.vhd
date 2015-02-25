1e4f88f3f5c08d657a0e2f7c2fa770b3832b8539
Meng-Yin <meng.tao@mail.mcgill.ca>
2015-02-24 22:45:26 -0500

    Update REG.vhd

--- VHDL code/REG.vhd
+++ VHDL code/REG.vhd
@@ -32,32 +32,32 @@ use ieee.numeric_std.all;
 
 entity REG is
 	port (
-		clk 						: in std_logic;
-		rst 						: in std_logic;
-		Rs, Rt, Rd					: in std_ulogic_vector(4 downto 0);
-		WB_data						: in signed(31 downto 0);
-		WB							: in std_logic;
-		A,B							: out signed(31 downto 0)
+		clk 				: in std_logic;
+		rst 				: in std_logic;
+		Rs, Rt, Rd			: in std_ulogic_vector(4 downto 0);
+		WB_data				: in signed(31 downto 0);
+		WB				: in std_logic;
+		A,B				: out signed(31 downto 0)
 	);
 end entity REG;
 
 architecture RTL of REG is
-TYPE StorageT IS ARRAY(0 TO 31) OF signed(31 DOWNTO 0); --Array for register storage
-SIGNAL registerfile : StorageT; --Register file
+TYPE StorageT IS ARRAY(0 TO 31) OF signed(31 DOWNTO 0); 				--Array for register storage
+SIGNAL registerfile : StorageT; 							--Register file
 begin
 	process(rst,clk)
 	begin
 		IF rst = '1' THEN 
 			FOR i IN 0 TO 31 LOOP
-				registerfile(i) <= (OTHERS => '0');-- Reset all the registers to 0 when rst is high
+				registerfile(i) <= (OTHERS => '0');			-- Reset all the registers to 0 when rst is high
 			END LOOP;
-		ELSIF rising_edge(clk) THEN			-- WRITE on rising edge!
+		ELSIF rising_edge(clk) THEN						-- WRITE on rising edge!
 			IF WB = '1' THEN
-				registerfile(to_integer(unsigned(Rd))) <= WB_data; -- Write the WB_data into the Rd register
+				registerfile(to_integer(unsigned(Rd))) <= WB_data; 	-- Write the WB_data into the Rd register
 			END IF;
 		END IF;
 	end process;
-	registerfile(0)<= (OTHERS => '0'); --Hard wire the register 0 to be 0
-	A <= registerfile(to_integer(unsigned(Rs))); -- output the data in the register address Rs and Rt
+	registerfile(0)<= (OTHERS => '0'); 						-- Hard wire the register 0 to be 0
+	A <= registerfile(to_integer(unsigned(Rs))); 					-- output the data in the register address Rs and Rt
 	B <= registerfile(to_integer(unsigned(Rt)));
 end architecture RTL;
