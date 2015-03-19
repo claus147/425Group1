3ac4a2132cd7bbd1ab0fa130950c1606b69798cc
Meng-Yin <meng.tao@mail.mcgill.ca>
2015-03-19 09:39:33 -0400

    add flush signal to ignore the instruction fetched

--- VHDL code/Reg_IF_ID.vhd
+++ VHDL code/Reg_IF_ID.vhd
@@ -4,6 +4,7 @@
 -- Inputs:
 -- - instr 32 bit operand
 -- - stall 1 bit control
+-- - flush 1 bit control (if branch is taken, must ignore what was fetched, ie stall due to branch) 
 -- - dirtyF, this is the OR of MemReadE with MemWriteE
 --
 -- Outputs:						
@@ -24,7 +25,7 @@ entity Reg_IF_ID is
 	port (
 		clk 				: in std_logic;
 		rst 				: in std_logic;
-		stall, dirtyF   :in std_logic;
+		stall, dirtyF, flush   :in std_logic;
     dirtyD   :out std_logic;
     instr : in std_ulogic_vector(31 downto 0);
 	  instr_out : out std_ulogic_vector(31 downto 0)
@@ -37,7 +38,7 @@ begin
   process(clk)
   begin
     
-	 if(rising_edge(clk) and stall = '0') then 
+	 if(rising_edge(clk) and stall = '0' and flush = '0') then 
 	   instr_out <= instr;
 	   dirtyD<=dirtyF;
 	 end if;
